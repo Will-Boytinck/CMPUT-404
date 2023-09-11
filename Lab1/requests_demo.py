f06e3a7571ceb6e2a412a77b8a1be1f47d49fe34
Will <48191282+Will-Boytinck@users.noreply.github.com>
2023-09-11 11:49:53 -0600

    Update requests_demo.py

--- Lab1/requests_demo.py
+++ Lab1/requests_demo.py
@@ -4,6 +4,9 @@ def main():
     print(requests.__version__)
     google_request = requests.get("https://www.google.com")
     print(google_request)
+    code_request = requests.get("https://raw.githubusercontent.com/Will-Boytinck/CMPUT-404/main/Lab1/requests_demo.py")
+    data = code_request.text
+    print(data)
     
 if __name__ == "__main__":
     main()
