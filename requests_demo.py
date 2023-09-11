1b6be27af7b3c1333b726bea16c1f5b27de49f04
Will <48191282+Will-Boytinck@users.noreply.github.com>
2023-09-11 11:39:27 -0600

    Delete requests_demo.py

--- requests_demo.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def main():
-    print(requests.__version__)
-    google_request = requests.get("https://www.google.com")
-    print(google_request)
-    
-if __name__ == "__main__":
-    main()
